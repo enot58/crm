c7f8be4021c3da0536b05b1acc14da512414ab95
RomanLaptop <t89875041999@yandex.ru>
2023-01-26 23:35:58 +0300

    Changes to be committed: 	modified:   client/src/components/MyNavBar.jsx 	modified:   client/src/pages/OneOrderPage.jsx

--- client/src/components/MyNavBar.jsx
+++ client/src/components/MyNavBar.jsx
@@ -10,15 +10,23 @@ const MyNavBar = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Домашняя</Nav.Link>
+                        <Nav.Link href="#link">Сформировать</Nav.Link>
+                        <Nav.Link href="#link">Все заявки</Nav.Link>
                         <Nav.Link href="#link">Каталог</Nav.Link>
+                        <Nav.Link href="#link">Склады</Nav.Link>
                         <NavDropdown title="Админ" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Админ</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1">Добавить пользователя</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
-                                Another action
+                                Добавить склад
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">
+                                Добавить тип заявки
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">
+                                Добавить объект
+                            </NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
+                            <NavDropdown.Item href="#action/3.5">
                                 Separated link
                             </NavDropdown.Item>
                         </NavDropdown>
--- client/src/pages/OneOrderPage.jsx
+++ client/src/pages/OneOrderPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Col, Container, Nav, Row, Tab, Tabs} from "react-bootstrap";
+import {Button, Col, Container, FloatingLabel, Form, Nav, Row, Tab, Tabs, Toast, ToastContainer} from "react-bootstrap";
 
 const OneOrderPage = () => {
     return (
@@ -14,7 +14,41 @@ const OneOrderPage = () => {
                     className="mb-3"
                 >
                     <Tab eventKey="home" title="Мастер">
-                        1
+                        <Row>
+                            <Col>
+                                <Form.Select aria-label="Default select example">
+                                    <option>Выбор типа оборудования</option>
+                                    <option value="1">Канализация</option>
+                                    <option selected="true" value="2">Отопление</option>
+                                    <option value="3">Вентиляция</option>
+                                </Form.Select>
+                            </Col>
+                            <Col>
+                                <Form.Select aria-label="Default select example">
+                                    <option>Выбор объекта</option>
+                                    <option value="1">Застава</option>
+                                    <option selected="true" value="2">Лугометрия</option>
+                                    <option value="3">Континент</option>
+                                </Form.Select>
+                            </Col>
+                            <Col>
+
+                            </Col>
+                        </Row>
+                        <Row className="mt-3">
+                            <FloatingLabel
+                                controlId="floatingTextarea"
+                                label="Comments"
+                                className="mb-3"
+                            >
+                                <Form.Control style={{ height: '200px' }} as="textarea" placeholder="Leave a comment here" />
+                            </FloatingLabel>
+                        </Row>
+
+                        <Row>
+                            <Button variant="success">Сохранить</Button>
+                        </Row>
+
                     </Tab>
                     <Tab eventKey="profile" title="Менеджер">
                         2
@@ -27,6 +61,26 @@ const OneOrderPage = () => {
                     </Tab>
                 </Tabs>
             </Row>
+            {/*<Row className="justify-content-end">
+                <ToastContainer position="bottom-end">
+                    <Toast>
+                        <Toast.Header>
+                            <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+                            <strong className="me-auto">Bootstrap</strong>
+                            <small className="text-muted">just now</small>
+                        </Toast.Header>
+                        <Toast.Body>See? Just like this.</Toast.Body>
+                    </Toast>
+                    <Toast>
+                        <Toast.Header>
+                            <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+                            <strong className="me-auto">Bootstrap</strong>
+                            <small className="text-muted">2 seconds ago</small>
+                        </Toast.Header>
+                        <Toast.Body>Heads up, toasts will stack automatically</Toast.Body>
+                    </Toast>
+                </ToastContainer>
+            </Row>*/}
         </Container>
     );
 };
