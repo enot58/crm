ede93d20f9619459eda095a1ebd21ee6c3a09e3c
Roman <t89875041999@yandex.ru>
2023-04-24 13:27:08 +0300

    Changes to be committed: 	modified:   server/src/user-description/user-description.service.ts
     Changes not staged for commit:
    	modified:   server/package-lock.json
    	modified:   server/package.json
    	modified:   server/src/app.module.ts
    	modified:   server/src/roles/roles.model.ts
    	modified:   server/src/user-description/dto/create-user-description.dto.ts
    	modified:   server/src/user-description/user-description.controller.ts
    	modified:   server/src/user-description/user-description.model.ts
    	modified:   server/src/user-description/user-description.module.ts
    	modified:   server/src/users/user.model.ts
    
     Untracked files:
    	server/src/files/

--- server/src/user-description/user-description.service.ts
+++ server/src/user-description/user-description.service.ts
@@ -1,4 +1,65 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { FilesService } from 'src/files/files.service';
+import { CreateUserDescriptionDto } from './dto/create-user-description.dto';
+import { UserDescription } from './user-description.model';
 
 @Injectable()
-export class UserDescriptionService {}
+export class UserDescriptionService {
+  constructor(
+    @InjectModel(UserDescription)
+    private userDescriptionRepository: typeof UserDescription,
+    private fileService: FilesService,
+  ) {}
+
+  async createUserDescription(dto: CreateUserDescriptionDto, image: any) {
+    const fileName = await this.fileService.createFile(image);
+    const userDescription = await this.userDescriptionRepository.create({
+      ...dto,
+      image: fileName,
+    });
+    return userDescription;
+  }
+
+  async getAllUserDescriptions() {
+    const userDescriptions = await this.userDescriptionRepository.findAll();
+    return userDescriptions;
+  }
+
+  async findUserDescriptionOneById(id: number) {
+    const userDescription = await this.userDescriptionRepository.findByPk(id);
+    return userDescription;
+  }
+
+  async deleteUserDescription(id: number) {
+    const userDescription = await this.userDescriptionRepository.destroy({
+      where: { id },
+    });
+    return userDescription;
+  }
+
+  async updateUserDescription(id: number, dto: CreateUserDescriptionDto) {
+    const userDescription = await this.userDescriptionRepository.findByPk(id);
+    userDescription.name = dto.name;
+    userDescription.lastName = dto.lastName;
+    userDescription.image = dto.image;
+    userDescription.post = dto.post;
+    userDescription.email = dto.email;
+    await userDescription.save();
+    return userDescription;
+  }
+
+  async findUserDescriptionByName(name: string) {
+    const userDescription = await this.userDescriptionRepository.findOne({
+      where: { name },
+    });
+    return userDescription;
+  }
+
+  async findUserDescriptionByEmail(email: string) {
+    const userDescription = await this.userDescriptionRepository.findOne({
+      where: { email },
+    });
+    return userDescription;
+  }
+}
