1c31f6ad0c53df2c2e0788243949ba2c33cc9c7a
Roman <t89875041999@yandex.ru>
2023-04-24 16:14:03 +0300

    Changes to be committed: 	modified:   server/src/user-description/user-description.controller.ts
     Changes not staged for commit:
    	modified:   server/src/files/files.service.ts
    	modified:   server/src/user-description/user-description.module.ts
    	modified:   server/src/user-description/user-description.service.ts
    	modified:   thunder-tests/thunderclient.json

--- server/src/user-description/user-description.controller.ts
+++ server/src/user-description/user-description.controller.ts
@@ -1,9 +1,16 @@
+import { RolesGuard } from 'src/auth/roles.guards';
+import { Roles } from 'src/auth/roles-auth.decorator';
 import { UserDescription } from './user-description.model';
 import { Controller, Body, Post, Get, UseInterceptors } from '@nestjs/common';
 import { ApiOperation, ApiTags, ApiResponse } from '@nestjs/swagger';
 import { CreateUserDescriptionDto } from './dto/create-user-description.dto';
 import { UserDescriptionService } from './user-description.service';
-import { UploadedFile } from '@nestjs/common/decorators';
+import {
+  Delete,
+  Param,
+  UploadedFile,
+  UseGuards,
+} from '@nestjs/common/decorators';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiTags('Описание пользователя')
@@ -13,6 +20,8 @@ export class UserDescriptionController {
   @ApiOperation({ summary: 'Создание описания пользователя' })
   @ApiResponse({ status: 201, type: UserDescription })
   @ApiResponse({ status: 404, description: 'Ошибка валидации' })
+  @Roles('admin')
+  @UseGuards(RolesGuard)
   @Post()
   @UseInterceptors(FileInterceptor('img'))
   create(@Body() dto: CreateUserDescriptionDto, @UploadedFile() image) {
@@ -22,8 +31,44 @@ export class UserDescriptionController {
   @ApiOperation({ summary: 'Получение всех описаний пользователей' })
   @ApiResponse({ status: 200, type: UserDescription })
   @ApiResponse({ status: 404, description: 'Ошибка валидации' })
+  @Roles('admin')
+  @UseGuards(RolesGuard)
   @Get()
   findAllDescription() {
     return this.userDescriptionService.getAllUserDescriptions();
   }
+
+  @ApiOperation({ summary: 'Удаление описания пользователя' })
+  @ApiResponse({ status: 200, type: UserDescription })
+  @ApiResponse({ status: 404, description: 'Ошибка валидации' })
+  @Roles('admin')
+  @UseGuards(RolesGuard)
+  @Delete(':id')
+  deleteDescription(@Param('id') id: number) {
+    return this.userDescriptionService.deleteUserDescription(id);
+  }
+
+  @ApiOperation({ summary: 'Получение описания пользователя по id' })
+  @ApiResponse({ status: 200, type: UserDescription })
+  @ApiResponse({ status: 404, description: 'Ошибка валидации' })
+  @Get(':id')
+  findOneDescription(@Param('id') id: number) {
+    return this.userDescriptionService.findUserDescriptionOneById(id);
+  }
+
+  @ApiOperation({ summary: 'Обновление описания пользователя' })
+  @ApiResponse({ status: 200, type: UserDescription })
+  @ApiResponse({ status: 404, description: 'Ошибка валидации' })
+  @Roles('admin')
+  @UseGuards(RolesGuard)
+  @Post(':id')
+  @UseInterceptors(FileInterceptor('img'))
+  update(
+    @Param('id') id: number,
+    @Body() dto: CreateUserDescriptionDto,
+    @UploadedFile() image,
+  ) {
+    return this.userDescriptionService.updateUserDescription(id, dto, image);
+  }
+  )
 }
